e4830a42f4e91a16a929f66c211babcef35210af
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-04 19:16:25 -0600

    se agrego una funcion en app.js y se utilizo en index

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ titulo.innerHTML = 'juego del numero secreto';
 let parrafo = document.querySelector('p');
 
 parrafo.innerHTML ="Indica un numero del 1 al 10";
+
+
+function intentoDeusuario(){
+    alert("click desde la funcion");
+}
