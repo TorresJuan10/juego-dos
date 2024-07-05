079319fb29e27d963141134be37ecaaf6fda4b92
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-04 18:48:23 -0600

    se actualizo app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'juego del numero secreto';
+
+
+let parrafo = document.querySelector('p');
+
+parrafo.innerHTML ="Indica un numero del 1 al 10";
