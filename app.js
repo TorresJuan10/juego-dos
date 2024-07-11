c14835ff535209599437781c5712ba93ffb793d4
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-11 17:10:05 -0600

    se agrego una funcion de numero random

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+let numeroSecreto = generarNumeroSecreto();
+
+console.log(numeroSecreto);
+
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 function intentoDeusuario(){
     alert("click desde la funcion");
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1
+    
 }
 
 asignarTextoElemento('h1','juego del numero secreto');
