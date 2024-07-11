734d616bee1429ec750e2cb7fff9e7e224a2f03e
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-11 16:52:04 -0600

    se agrego una funcion para reducir codigo

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'juego del numero secreto';
 
-
-let parrafo = document.querySelector('p');
-
-parrafo.innerHTML ="Indica un numero del 1 al 10";
-
-
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 function intentoDeusuario(){
     alert("click desde la funcion");
 }
+
+asignarTextoElemento('h1','juego del numero secreto');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
