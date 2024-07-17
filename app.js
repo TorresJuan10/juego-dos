ecb868360d99afc8b755c33a9f298fbd27c27e00
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-16 19:00:45 -0600

    se agrego la funcion vaciar caja cada intento

--- app.js
+++ app.js
@@ -14,18 +14,34 @@ function verificarIntento() {
    
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p', `acertaste el numero en ${intentos} ${(intentos == 1) ? 'intento' : 'intentos'}`);
+
+        //boton de nuevo juego
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        
     } else {
+        //El usuario no acerto
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p', 'El numero secreto es menor');
         } else {
             asignarTextoElemento('p', 'El numero secreto es mayor');
         }
-
         intentos++;
-    
+        limpiarCaja();
     }
     return;
 }
+
+
+    function limpiarCaja() {
+        //opcion 1
+        /*
+        let valorCaja = document.querySelector('#valorUsuario');
+        valorCaja.value = '';
+        */
+
+        //opcion 2 
+        document.querySelector('#valorUsuario').value = '';
+    }
 
     function generarNumeroSecreto() {
         return Math.floor(Math.random() * 10) + 1
