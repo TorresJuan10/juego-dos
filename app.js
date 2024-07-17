d5c9e8b2131fd2969399925af7574030b0d5251d
TorresJuan10 <jesus.alaniz.t@gmail.com>
2024-07-16 18:29:53 -0600

    se agrego un contador

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
-
-
+let intentos = 1;
+ console.log(numeroSecreto);
 
 
 function asignarTextoElemento(elemento, texto) {
@@ -8,21 +8,29 @@ function asignarTextoElemento(elemento, texto) {
     elementoHTML.innerHTML = texto;
     return;
 }
-function verificarIntento(){
+function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroDeUsuario);
-    console.log(numeroDeUsuario === numeroSecreto);
 
+   
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `acertaste el numero en ${intentos} ${(intentos == 1) ? 'intento' : 'intentos'}`);
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El numero secreto es menor');
+        } else {
+            asignarTextoElemento('p', 'El numero secreto es mayor');
+        }
+
+        intentos++;
+    
+    }
     return;
 }
 
-function generarNumeroSecreto() {
-    return Math.floor(Math.random()*10)+1
-    
-}
+    function generarNumeroSecreto() {
+        return Math.floor(Math.random() * 10) + 1
 
-asignarTextoElemento('h1','juego del numero secreto');
-asignarTextoElemento('p','Indica un numero del 1 al 10');
+    }
+
+    asignarTextoElemento('h1', 'juego del numero secreto');
+    asignarTextoElemento('p', 'Indica un numero del 1 al 10');
